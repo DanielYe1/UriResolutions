becfdc5105f60b44e2eded4234daaf711fce7857
Daniel Ye <danielxinxin1@gmail.com>
2017-05-14 00:19:20 -0300

    adding uri 1709 data structure exercise

--- C++Resolutions/DataStructures/1709.cpp
+++ C++Resolutions/DataStructures/1709.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 int main() {
-    int size;
-    cin >> size;
+    int total;
+    scanf("%d", &total);
+    int n = total / 2;
+    int atual = total;
+    int count = 0;
+
+    do {
+        if (atual > n) {
+            atual = 2 * (atual - 1 - n) + 1;
+        } else if (atual <= n) {
+            atual *= 2;
+        }
+        count += 1;
+    } while (atual != total);
+    cout << count << endl;
 
     return 0;
 }
